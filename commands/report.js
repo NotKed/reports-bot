a8b401d39b5fe6bebd17d50705988e49424b77af
Kyle Edwards <kyleedwardsdev@gmail.com>
2020-02-01 02:57:50 +0000

    command setup

---
+++ commands/report.js
@@ -0,0 +1,11 @@
+module.exports = class apply {
+  constructor() {
+    this.name = 'report',
+    this.usage = '-report [username] [evidence] [reason]',
+    this.alias = []
+  }
+
+  run(bot, message, args) {
+    
+  }
+}
--- index.js
+++ index.js
@@ -1,8 +1,36 @@
+// Created by NotKed for Zeldaria Development
+// 01/02/2020 - 02:49
+
 const Discord = require('discord.js');
-const bot = new Discord.Client();
+const bot = new Discord.Client({disableEveryone: true});
 
-bot.on('ready', () => {
+require('dotenv').config();
 
+const { CommandHandler } = require('djs-commands');
+const CH = new CommandHandler({
+    folder: __dirname + "/commands/",
+    prefix: [process.env.PREFIX]
 });
 
-bot.login();
+bot.on('ready', () => {
+  console.log(`${bot.user.username} is online.`);
+});
+
+bot.on('message', message => {
+
+    if(message.channel.type === 'dm') return;
+    if(message.author.type === 'bot') return;
+    let args = message.content.split(" ");
+    let command = args[0];
+    args = args.splice(1);
+    let cmd = CH.getCommand(command);
+    if(!cmd) return;
+
+    try {
+        cmd.run(bot, message, args);
+    } catch(e) {
+        console.log(e);
+    }
+});
+
+bot.login(process.env.TOKEN);
